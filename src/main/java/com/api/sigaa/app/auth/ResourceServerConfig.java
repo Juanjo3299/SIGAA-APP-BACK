3513a73deb1f64e06357b1cc7d57696e6f58afe3
Juan Mendez <juanjo3299@gmail.com>
2020-10-17 23:14:55 -0500

    OPtimizacion de consultas hacia la bd

--- src/main/java/com/api/sigaa/app/auth/ResourceServerConfig.java
+++ src/main/java/com/api/sigaa/app/auth/ResourceServerConfig.java
@@ -33,7 +33,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost", "http://localhost:8100", "https://sigaa-app-back.herokuapp.com"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost", "http://localhost:8100"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
