2496f6717e0d2f69fb7d04aeeb63adf534c6d24c
Juan Mendez <juanjo3299@gmail.com>
2020-10-18 02:45:30 -0500

    Cors

--- src/main/java/com/api/sigaa/app/SigaaAppBackApplication.java
+++ src/main/java/com/api/sigaa/app/SigaaAppBackApplication.java
@@ -3,25 +3,12 @@ package com.api.sigaa.app;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SigaaAppBackApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SigaaAppBackApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/oauth/token").allowedOrigins("http://localhost:4200");
-            }
-        };
     }
 
     @Override
