4a2d9686493892659574ad951a7e8d4a839d3612
Juan Mendez <juanjo3299@gmail.com>
2020-10-18 02:42:35 -0500

    Cors

--- src/main/java/com/api/sigaa/app/SigaaAppBackApplication.java
+++ src/main/java/com/api/sigaa/app/SigaaAppBackApplication.java
@@ -19,7 +19,7 @@ public class SigaaAppBackApplication implements CommandLineRunner {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*/*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/oauth/token").allowedOrigins("http://localhost:4200");
             }
         };
     }
